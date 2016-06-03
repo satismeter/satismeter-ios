4d98ce53d58079b625b53b20b7cc4bfa3ed4402b
Esat Pllana <pllanaesat@gmail.com>
2016-06-03 22:22:00 +0200

    updated main header file

--- Pod/Classes/SatisMeter.h
+++ Pod/Classes/SatisMeter.h
@@ -2,7 +2,7 @@
 //  SatisMeter.h
 //  SatisMeter
 //
-//  Developed by Appsix Mobile on 2/24/16.
+//  Created by Esat Pllana on 2/24/16.
 //  Copyright © 2016 SatisMeter (satismeter.com). All rights reserved.
 //
 
@@ -10,11 +10,10 @@
 #import <UIKit/UIKit.h>
 @protocol SatisMeterCoreDelegate <NSObject>
 @optional
-
-/// This method is called when the SatisView is shown
+/// delegate method when the view is shown
 -(void)SatisMeterViewDidShow;
 
-/// This method is called when the SatisView is dismissed
+/// delegate method when the view is dismissed
 -(void)SatisMeterViewDidDismiss;
 @end
 
@@ -23,16 +22,18 @@
 +(instancetype) sharedInstance;
 @property (nonatomic,weak) id delegate;
 
-/// This is the authentication method and needs to be called before any other method. Parameter "traits" can be valued by your preferences
--(void)identifyUserWithUserId:(NSString*)Id writeKey:(NSString*)writeKey andTraitsDictionary:(NSDictionary*)traits;
+@property BOOL shouldShowSatisMeterOnActive;
+@property BOOL isShown;
 
-/// This is used in case server url needs to be modified
+
+
+/// identify user with user_id, writeKey and traits dictionary if applicable
+-(void)identifyUserWithUserId:(NSString*)Id writeKey:(NSString*)writeKey andTraitsDictionary:(NSDictionary*)traits;
+/// identify user with user_id, writeKey, traits dictionary if applicable and custom server url
 -(void)identifyUserWithUserId:(NSString*)Id writeKey:(NSString*)writeKey andTraitsDictionary:(NSDictionary*)traits andServerUrl:(NSString*)serverUrl;
 
-/// The default value is YES, if you want to show SatisMeter view manually then set it NO
-@property BOOL shouldShowSatisMeterOnActive;
 
-/// Method called to show the Satis Meter view
+/// show satismeter view
 -(void) showSatisMeterView;
 
 @end
