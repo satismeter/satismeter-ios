8846ad6a103aeb0e4e9764a4c9234f71d4a37276
Jakub Sedlacek <sedlacek.jakub@gmail.com>
2018-11-23 13:55:03 +0100

    Add swift example

---
+++ Examples/Swift/satismeter-swift-test/Bridging-Header.h
@@ -0,0 +1,14 @@
+//
+//  Bridging-Header.h
+//  satismeter-swift-test
+//
+//  Created by Jakub Sedlacek on 14/11/2018.
+//  Copyright © 2018 Jakub Sedlacek. All rights reserved.
+//
+
+#ifndef Bridging_Header_h
+#define Bridging_Header_h
+
+#import <SatisMeter/SatisMeter.h>
+
+#endif /* Bridging_Header_h */
---
+++ Examples/Swift/satismeter-swift-test/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  satismeter-swift-test
+//
+//  Created by Jakub Sedlacek on 14/11/2018.
+//  Copyright © 2018 Jakub Sedlacek. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
